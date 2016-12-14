8b1fa96da1de1829e3b7ca2314325d74673a9a1e
capellaresumo <capellaresumo@gmail.com>
2016-12-14 14:54:50 -0200

    1481734490

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const net = require('net');
 const server = net.createServer( function (client) {
+const spawn = require('child_process').spawn;
   // 'connection' listener
   console.log('client connected');
 
@@ -8,8 +9,14 @@ const server = net.createServer( function (client) {
     });
 
     client.on('data', function (data) {
-        console.log(data.toString());
+        var string = data.toString();
         client.end();
+        openssl = spawn('openssl', ['rsautl', '-decrypt', '-inkey', 'server_private_key.pem']);
+        openssl.stdin.write(data);
+        openssl.stdin.end();
+        openssl.stdout.on('data', function (data) {
+            console.log(data;
+        });
     });
 
 });
